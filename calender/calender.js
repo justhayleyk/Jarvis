58ab110c08058331aeaee2d8b0194cf67f62cabb
Suraj Jagdev <suraj_57@hotmail.com>
2018-09-30 19:29:30 -0400

    modified calender .js

--- calender/calender.js
+++ calender/calender.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 var express = require('express');
 var app = express();
-var googleapis = require('googleapis');
+const { google } = require('googleapis');
 var path = require('path');
 module.exports = function(app) {
   //will open up static page for login
@@ -9,23 +9,24 @@ module.exports = function(app) {
     res.sendFile(path.join(__dirname, '../views/calender-login.html'));
 
     const oauth2Client = new google.auth.OAuth2(
-      YOUR_CLIENT_ID,
-      YOUR_CLIENT_SECRET,
-      YOUR_REDIRECT_URL
+      '32898389523-n1q62hpuivjjee3i86n876pf96vkn543.apps.googleusercontent.com',
+      'h_XMVDN_1aTuCOhqRLwdFTUt',
+      'http://localhost:3000/oauthcallback'
     );
 
     // generate a url that asks permissions for Google+ and Google Calendar scopes
-    const scopes = [
-      'https://www.googleapis.com/auth/plus.me',
-      'https://www.googleapis.com/auth/calendar'
-    ];
+    const scopes = ['https://www.googleapis.com/auth/calendar'];
 
     const url = oauth2Client.generateAuthUrl({
       // 'online' (default) or 'offline' (gets refresh_token)
       access_type: 'offline',
 
       // If you only need one scope you can pass it as a string
-      scope: scopes
+      scope: scopes,
+      prompt: 'consent'
+    });
+    app.get('/url', function(req, res) {
+      res.send(url);
     });
   });
 };
