acc649b6d8647dbb3f01d9f7ca42c4106b2080e7
Suraj Jagdev <suraj_57@hotmail.com>
2018-10-01 02:22:55 -0400

    so far got access token, tokentypes, scopes and expiration token ; now need to access these creds of user and modify calender

--- calender/calender.js
+++ calender/calender.js
@@ -7,26 +7,41 @@ module.exports = function(app) {
   //will open up static page for login
   app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname, '../views/calender-login.html'));
+  });
 
-    const oauth2Client = new google.auth.OAuth2(
-      process.env.CLIENT_ID,
-      process.env.CLIENT_SECRET_ID,
-      process.env.URI
+  /*const oauth2Client = new google.auth.OAuth2(
+    process.env.CLIENT_ID,
+    process.env.CLIENT_SECRET_ID,
+    process.env.URI
+  );
+
+  // generate a url that asks permissions for Google+ and Google Calendar scopes
+  const scopes = ['https://www.googleapis.com/auth/calendar.events'];
+
+  const url = oauth2Client.generateAuthUrl({
+    // 'online' (default) or 'offline' (gets refresh_token)
+    access_type: 'offline',
+
+    // If you only need one scope you can pass it as a string
+    scope: scopes,
+    prompt: 'consent'
+  });*/
+
+  app.post('/oauthcallback', function(req, res) {
+    /*console.log(res.data.access_token);
+    console.log(res.data.scope_token);
+    console.log(res.data.token_type);
+    console.log(res.data.expires_in);*/
+    /*console.log('AccessToken: ' + req.body.access_token);
+    console.log('scope: ' + req.body.scope);
+    console.log(' tokentype: ' + req.body.token_type);
+    console.log('expires in ' + req.body.expires_in);*/
+    var accessToken = req.body.access_token;
+    var scope = req.body.scope;
+    var tokenType = req.body.token_type;
+    var expiresIn = req.body.expires_in;
+    console.log(
+      `this is access token ${accessToken}. This is scope ${scope}. This is token type ${tokenType}. This is expires in ${expiresIn}`
     );
-
-    // generate a url that asks permissions for Google+ and Google Calendar scopes
-    const scopes = ['https://www.googleapis.com/auth/calendar'];
-
-    const url = oauth2Client.generateAuthUrl({
-      // 'online' (default) or 'offline' (gets refresh_token)
-      access_type: 'offline',
-
-      // If you only need one scope you can pass it as a string
-      scope: scopes,
-      prompt: 'consent'
-    });
-    app.get('/url', function(req, res) {
-      res.send(url);
-    });
   });
 };
