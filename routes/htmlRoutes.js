81672520b1f771ac2da7f02b2c1e90e7f2cd02ae
Suraj Jagdev <suraj_57@hotmail.com>
2018-10-09 19:44:15 -0400

    added db user customer to db

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -2,7 +2,10 @@ var db = require('../models');
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
-
+var expressValidator = require('express-validator');
+var bcrypt = require('bcrypt-nodejs');
+var saltRounds = process.env.SALT;
+var salt = bcrypt.genSaltSync(saltRounds);
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
@@ -48,20 +51,41 @@ module.exports = function(app) {
     // });
   });
   app.post('/signup/', function(req, res) {
-    console.log(req.body.userName);
-    console.log(req.body.userPassword);
-    console.log(req.body.name);
+    //var custname=req.body.userName;
+    var custpswrd = req.body.userPassword;
+    /*console.log(req.body.name);
     console.log(req.body.userPhone);
     console.log(req.body.userAddress);
-    console.log(req.body.userEmail);
-    db.Customer.create({
-      custname: req.body.userName,
-      custaddress: req.body.userAddress,
-      custphone: req.body.userPhone,
-      custemail: req.body.userEmail,
-      custpswrd: req.body.userPassword
-    }).then(function(newUser) {
-      console.log(newUser);
+    console.log(req.body.userEmail);*/
+    req.checkBody('name', 'Name cannot be empty. ').notEmpty();
+    req.checkBody('userName', 'Username cannot be empty.').notEmpty();
+    req
+      .checkBody('userName', 'Username has t be between 4 and 15 characters.')
+      .len(4, 15);
+    req.checkBody('userAddress', 'Address cannot be empty. ').notEmpty();
+    req.checkBody('userPhone', 'Cannot be empty. ').notEmpty();
+    req.checkBody('userPhone', 'Must be numbers only').isNumeric();
+    req.checkBody('userPhone', 'Must be atleast 10 digits').len(10, 15);
+    req.checkBody('userEmail', 'Cannot be empty').notEmpty();
+    req.checkBody('userEmail', 'Email entered is invalid').isEmail();
+    req.checkBody('userPassword', 'Cannot be empty').notEmpty();
+    req.checkBody('userEmail', 'between 4 and 60 characters').len(4, 60);
+    var errors = req.validationErrors();
+    if (errors) {
+      console.log(`Errors: ${JSON.stringify(errors)}`);
+      res.render('signup', { errors: errors });
+    }
+    bcrypt.hash(custpswrd, salt, null, function(err, hash) {
+      db.Customer.create({
+        custname: req.body.name,
+        custusername: req.body.userName,
+        custaddress: req.body.userAddress,
+        custphone: req.body.userPhone,
+        custemail: req.body.userEmail,
+        custpswrd: custpswrd
+      }).then(function(newUser) {
+        console.log('hello' + newUser);
+      });
     });
   });
 
