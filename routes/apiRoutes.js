6e1dcba16d3e973e277b7fd6480ecaf2bd9f1fca
TheBetterMan <Rob.wawrzak@hotmail.com>
2018-10-09 18:48:54 -0400

    Sign-up submitBtn, API routes for AJAX calls (up for rivision), and signUp JS

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,29 +1,62 @@
-//Will need to change modules name (after db) to Contractor, Customer or User
+//Will need to change modules name (after db) to Contractor, Customer or Jobs
 //
 //
 var db = require('../models');
 
 module.exports = function(app) {
-  // Get all examples
-  app.get('/api/examples', function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.json(dbExamples);
+  // Contractor sign up
+  app.post('/api/contractor/SignUp', function(req, res) {
+    db.Contractor.create(req.body).then(function(dbSignUp) {
+      res.json(dbSignUp);
     });
   });
 
-  // Create a new example
-  app.post('/api/examples', function(req, res) {
-    db.Example.create(req.body).then(function(dbExample) {
-      res.json(dbExample);
+  // Customer sign up
+  app.post('/api/customer/SignUp', function(req, res) {
+    db.Customer.create(req.body).then(function(dbSignUp) {
+      res.json(dbSignUp);
+    });
+  });
+
+  // Contractor Login
+  app.get('/api/contractor/Login', function(req, res) {
+    db.Contractor.create(req.body).then(function(login) {
+      res.json(login);
+    });
+  });
+
+  // Customer Login
+  app.get('/api/customer/Login', function(req, res) {
+    db.Customer.create(req.body).then(function(login) {
+      res.json(login);
+    });
+  });
+
+  // Contractor create new post
+  app.post('/api/contractorPost', function(req, res) {
+    db.Job.create(req.body).then(function(dbPost) {
+      res.json(dbPost);
+    });
+  });
+
+  // Customer create new post
+  app.post('/api/customerPost', function(req, res) {
+    db.Job.create(req.body).then(function(dbPost) {
+      res.json(dbPost);
+    });
+  });
+
+  // Customer create new post
+  app.get('/api/Jobs', function(req, res) {
+    db.Job.findAll({}).then(function(dbPost) {
+      res.json(dbPost);
     });
   });
 
   // Delete an example by id
-  app.delete('/api/examples/:id', function(req, res) {
-    db.Example.destroy({ where: { id: req.params.id } }).then(function(
-      dbExample
-    ) {
-      res.json(dbExample);
+  app.delete('/api/Jobs/:id', function(req, res) {
+    db.Job.destroy({ where: { id: req.params.id } }).then(function(dbDelete) {
+      res.json(dbDelete);
     });
   });
 };
